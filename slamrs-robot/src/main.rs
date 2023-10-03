c0c36f3df23b570b90d37b5dfa1b089110fe447b
antbern <40672068+antbern@users.noreply.github.com>
2023-10-03 18:12:51 +0200

    Implement neato serial parsing and driver, bump dependencies

--- slamrs-robot/src/main.rs
+++ slamrs-robot/src/main.rs
@@ -2,30 +2,53 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
+use core::sync::atomic::{AtomicBool, AtomicU16, Ordering};
+
 use embassy_executor::_export::StaticCell;
 use embassy_net::tcp::TcpSocket;
-use embassy_net::{Config, IpListenEndpoint, Ipv4Address, Stack, StackResources};
+use embassy_net::{Config, IpListenEndpoint, Stack, StackResources};
 
+use embassy_sync::channel::{Channel, Receiver, Sender};
+use embedded_io::asynch::Write;
 use esp32_hal as hal;
 
 use embassy_executor::Executor;
+use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
 use embassy_time::{Duration, Timer};
 use embedded_svc::wifi::{ClientConfiguration, Configuration, Wifi};
 use esp_backtrace as _;
-use esp_println::logger::init_logger;
-use esp_println::{print, println};
-use esp_wifi::initialize;
+use esp_println::println;
 use esp_wifi::wifi::{WifiController, WifiDevice, WifiEvent, WifiMode, WifiState};
+use esp_wifi::{initialize, EspWifiInitFor};
 use hal::clock::{ClockControl, CpuClock};
-use hal::gpio::{AnyPin, Output, PushPull};
+use hal::gpio::Unknown;
+use hal::gpio::{AnyPin, GpioPin, Output, PushPull};
+use hal::mcpwm::operator::PwmPinConfig;
+use hal::mcpwm::timer::PwmWorkingMode;
+use hal::mcpwm::{PeripheralClockConfig, MCPWM};
+use hal::peripherals::{Interrupt, UART2};
+use hal::uart::config::{DataBits, Parity, StopBits};
+use hal::uart::{self, TxRxPins, Uart};
 use hal::{embassy, peripherals::Peripherals, prelude::*, timer::TimerGroup, Rtc};
-use hal::{Rng, IO};
+use hal::{interrupt, Rng, IO};
 
-#[cfg(any(feature = "esp32c3", feature = "esp32c2", feature = "esp32c6"))]
-use hal::system::SystemExt;
+use smoltcp::socket::tcp;
+
+type NeatoPwmPin = esp32_hal::mcpwm::operator::PwmPin<
+    'static,
+    GpioPin<Unknown, 27>,
+    esp32_hal::peripherals::MCPWM0,
+    0,
+    true,
+>;
 
 const SSID: &str = env!("SSID");
 const PASSWORD: &str = env!("PASSWORD");
+
+static LAST_RPM: AtomicU16 = AtomicU16::new(0);
+static MOTOR_ON: AtomicBool = AtomicBool::new(false);
+
+static CHANNEL: Channel<CriticalSectionRawMutex, [u8; 1980], 10> = Channel::new();
 
 macro_rules! singleton {
     ($val:expr) => {{
@@ -40,31 +63,52 @@ static EXECUTOR: StaticCell<Executor> = StaticCell::new();
 
 #[entry]
 fn main() -> ! {
-    init_logger(log::LevelFilter::Info);
-    esp_wifi::init_heap();
+    esp_println::logger::init_logger(log::LevelFilter::Info);
+
+    // esp_wifi::init_heap();
 
     let peripherals = Peripherals::take();
 
-    let system = peripherals.DPORT.split();
+    let mut system = peripherals.DPORT.split();
 
     let clocks = ClockControl::configure(system.clock_control, CpuClock::Clock240MHz).freeze();
+
     let mut rtc = Rtc::new(peripherals.RTC_CNTL);
     rtc.rwdt.disable();
 
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
     let mut led = io.pins.gpio2.into_push_pull_output().degrade();
-
     led.set_high().unwrap();
 
-    let timer = TimerGroup::new(peripherals.TIMG1, &clocks).timer0;
-    initialize(timer, Rng::new(peripherals.RNG), &clocks).unwrap();
+    let timer = TimerGroup::new(
+        peripherals.TIMG1,
+        &clocks,
+        &mut system.peripheral_clock_control,
+    )
+    .timer0;
 
-    let (wifi_interface, controller) = esp_wifi::wifi::new(WifiMode::Sta);
+    let init = initialize(
+        EspWifiInitFor::Wifi,
+        timer,
+        Rng::new(peripherals.RNG),
+        system.radio_clock_control,
+        &clocks,
+    )
+    .unwrap();
 
-    let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
+    let (wifi, ..) = peripherals.RADIO.split();
+    let (wifi_interface, controller) =
+        esp_wifi::wifi::new_with_mode(&init, wifi, WifiMode::Sta).unwrap();
+
+    let timer_group0 = TimerGroup::new(
+        peripherals.TIMG0,
+        &clocks,
+        &mut system.peripheral_clock_control,
+    );
+
     embassy::init(&clocks, timer_group0.timer0);
 
-    let config = Config::Dhcp(Default::default());
+    let config = Config::dhcpv4(Default::default());
 
     let seed = 1234; // very random, very secure seed
 
@@ -76,16 +120,75 @@ fn main() -> ! {
         seed
     ));
 
+    // setup serial port to talk to the Neato Lidar
+    let pins = TxRxPins::new_tx_rx(
+        io.pins.gpio17.into_push_pull_output(),
+        io.pins.gpio16.into_floating_input(),
+    );
+    // setup serial port
+    let config = uart::config::Config {
+        baudrate: 115200,
+        data_bits: DataBits::DataBits8,
+        parity: Parity::ParityNone,
+        stop_bits: StopBits::STOP1,
+    };
+
+    let mut uart2 = Uart::new_with_config(
+        peripherals.UART2,
+        Some(config),
+        Some(pins),
+        &clocks,
+        &mut system.peripheral_clock_control,
+    );
+    uart2
+        .set_rx_fifo_full_threshold(64)
+        .expect("Could not set rx fifo full threshold");
+
+    //////////////////
+    // initialize PWM motor controlling pin (GPIO 27)
+    let pin = io.pins.gpio27;
+
+    // initialize peripheral
+    let clock_cfg = PeripheralClockConfig::with_frequency(&clocks, 40u32.MHz()).unwrap();
+    let mut mcpwm = MCPWM::new(
+        peripherals.MCPWM0,
+        clock_cfg,
+        &mut system.peripheral_clock_control,
+    );
+
+    // connect operator0 to timer0
+    mcpwm.operator0.set_timer(&mcpwm.timer0);
+    // connect operator0 to pin
+    let pwm_pin = mcpwm
+        .operator0
+        .with_pin_a(pin, PwmPinConfig::UP_ACTIVE_HIGH);
+
+    interrupt::enable(Interrupt::UART2, interrupt::Priority::Priority3).unwrap();
+
+    // start timer with timestamp values in the range of 0..=99 and a frequency of
+    // 20 kHz
+    let timer_clock_cfg = clock_cfg
+        .timer_clock_with_frequency(99, PwmWorkingMode::Increase, 20u32.kHz())
+        .unwrap();
+    mcpwm.timer0.start(timer_clock_cfg);
+
+    // channel to share messages
+
+    let sender = CHANNEL.sender();
+    let receiver = CHANNEL.receiver();
+
     let executor = EXECUTOR.init(Executor::new());
     executor.run(|spawner| {
         spawner.spawn(connection(controller)).ok();
         spawner.spawn(net_task(&stack)).ok();
-        spawner.spawn(task(&stack, led)).ok();
+        spawner.spawn(task(&stack, led, receiver)).ok();
+        spawner.spawn(neato_serial_read(uart2, sender)).ok();
+        spawner.spawn(motor_control(pwm_pin)).ok();
     });
 }
 
 #[embassy_executor::task]
-async fn connection(mut controller: WifiController) {
+async fn connection(mut controller: WifiController<'static>) {
     println!("start connection task");
     println!("Device capabilities: {:?}", controller.get_capabilities());
     loop {
@@ -121,12 +224,167 @@ async fn connection(mut controller: WifiController) {
 }
 
 #[embassy_executor::task]
-async fn net_task(stack: &'static Stack<WifiDevice>) {
+async fn net_task(stack: &'static Stack<WifiDevice<'static>>) {
     stack.run().await
 }
 
+async fn connected_loop(
+    socket: &mut TcpSocket<'_>,
+    receiver: &mut Receiver<'static, CriticalSectionRawMutex, [u8; 1980], 10>,
+) {
+    while socket.state() == tcp::State::Established {
+        // read incoming messages (to enable or disable the motor)
+        if socket.can_recv() {
+            let mut buffer = [0; 16];
+            if let Ok(len) = socket.read(&mut buffer).await {
+                if len > 0 {
+                    match buffer[0] {
+                        b'A' => {
+                            MOTOR_ON.store(true, Ordering::Relaxed);
+                            LAST_RPM.store(0, Ordering::Relaxed);
+                        }
+                        b'D' => {
+                            MOTOR_ON.store(false, Ordering::Relaxed);
+                        }
+                        _ => {}
+                    }
+                }
+            }
+        }
+
+        // ping message to keep the connection alive
+        let buffer = [0xff; 1];
+        socket.write_all(&buffer).await.ok();
+
+        // process any parsed packets and send them via the socket
+        if let Ok(packet) = receiver.try_recv() {
+            socket.write_all(&packet).await.ok();
+        }
+
+        Timer::after(Duration::from_millis(50)).await;
+    }
+
+    MOTOR_ON.store(false, Ordering::Relaxed);
+}
+
+/// Task that always reads the current PWM from the neato lidar,
 #[embassy_executor::task]
-async fn task(stack: &'static Stack<WifiDevice>, mut led: AnyPin<Output<PushPull>>) {
+async fn neato_serial_read(
+    mut neato: Uart<'static, UART2>,
+    sender: Sender<'static, CriticalSectionRawMutex, [u8; 1980], 10>,
+) {
+    // to hold the whole packet
+    let mut buffer = [0u8; 1980];
+    let mut state = State::LookingForStart;
+    let mut last_rpm = 0;
+    let mut last_byte = 0u8;
+
+    let mut buff = [0u8; 64];
+
+    loop {
+        while let Ok(len) = embedded_io_async::Read::read(&mut neato, &mut buff).await {
+            for i in 0..len {
+                let read = buff[i];
+
+                match state {
+                    State::LookingForStart => {
+                        if last_byte == 0xFA && read == 0xA0 {
+                            buffer[0] = last_byte;
+                            buffer[1] = read;
+                            state = State::CollectingBytes { index: 2 };
+                        }
+                    }
+                    State::CollectingBytes { index } => {
+                        buffer[index] = read;
+
+                        if index < buffer.len() - 1 {
+                            state = State::CollectingBytes { index: index + 1 };
+                        } else {
+                            state = State::ParseRPM;
+                        }
+                    }
+                    State::ParseRPM => {
+                        let rpm_low = buffer[2];
+                        let rpm_high = buffer[3];
+
+                        let mut rpm = ((rpm_high as u16) << 8) | (rpm_low as u16);
+
+                        if (rpm as i16 - last_rpm as i16).abs() > 100 {
+                            rpm = last_rpm;
+                        }
+                        last_rpm = rpm;
+
+                        // if rpm >= 0 && rpm <
+
+                        LAST_RPM.store(rpm / 64, core::sync::atomic::Ordering::Relaxed);
+
+                        // write the full packet to a shared Channel or Pipe
+                        // https://docs.embassy.dev/embassy-sync/git/default/index.html
+                        sender
+                            .try_send(buffer)
+                            .expect("Could not send parsed packet");
+
+                        state = State::LookingForStart;
+                    }
+                }
+
+                last_byte = read;
+
+                // println!("0x{read:02x}");
+            }
+        }
+    }
+}
+
+#[embassy_executor::task]
+async fn motor_control(mut pwm_pin: NeatoPwmPin) {
+    let mut pwm_current: i16 = 0;
+
+    loop {
+        Timer::after(Duration::from_millis(150)).await;
+
+        let rpm_target = if MOTOR_ON.load(Ordering::Relaxed) {
+            300
+        } else {
+            0
+        };
+
+        let last_rpm = LAST_RPM.load(Ordering::Relaxed);
+
+        let error = rpm_target as i16 - last_rpm as i16;
+
+        pwm_current += error;
+
+        // cap the allowed PWM range
+        pwm_current = pwm_current.clamp(0, 90 * 100);
+
+        let mut pwm = pwm_current / 100;
+
+        if rpm_target == 0 {
+            pwm = 0;
+        }
+
+        pwm_pin.set_timestamp(pwm as u16);
+
+        println!(
+            "Control, {} rpm, error={error}. New PWM = {}",
+            last_rpm, pwm
+        );
+    }
+}
+#[derive(Debug)]
+enum State {
+    LookingForStart,
+    CollectingBytes { index: usize },
+    ParseRPM,
+}
+
+#[embassy_executor::task]
+async fn task(
+    stack: &'static Stack<WifiDevice<'static>>,
+    mut led: AnyPin<Output<PushPull>>,
+    mut receiver: Receiver<'static, CriticalSectionRawMutex, [u8; 1980], 10>,
+) {
     let mut rx_buffer = [0; 4096];
     let mut tx_buffer = [0; 4096];
 
@@ -142,7 +400,7 @@ async fn task(stack: &'static Stack<WifiDevice>, mut led: AnyPin<Output<PushPull
 
     println!("Waiting to get IP address...");
     loop {
-        if let Some(config) = stack.config() {
+        if let Some(config) = stack.config_v4() {
             println!("Got IP: {}", config.address);
             break;
         }
@@ -155,7 +413,7 @@ async fn task(stack: &'static Stack<WifiDevice>, mut led: AnyPin<Output<PushPull
     Timer::after(Duration::from_millis(1_000)).await;
 
     let mut socket = TcpSocket::new(&stack, &mut rx_buffer, &mut tx_buffer);
-    socket.set_timeout(Some(embassy_net::SmolDuration::from_secs(10)));
+    socket.set_timeout(Some(embassy_time::Duration::from_secs(10)));
 
     loop {
         println!("Wait for connection...");
@@ -173,49 +431,7 @@ async fn task(stack: &'static Stack<WifiDevice>, mut led: AnyPin<Output<PushPull
             continue;
         }
 
-        use embedded_io::asynch::Write;
-
-        let mut buffer = [0u8; 1024];
-        let mut pos = 0;
-        loop {
-            match socket.read(&mut buffer).await {
-                Ok(0) => {
-                    println!("read EOF");
-                    break;
-                }
-                Ok(len) => {
-                    let to_print =
-                        unsafe { core::str::from_utf8_unchecked(&buffer[..(pos + len)]) };
-
-                    if to_print.contains("\r\n\r\n") {
-                        print!("{}", to_print);
-                        println!();
-                        break;
-                    }
-
-                    pos += len;
-                }
-                Err(e) => {
-                    println!("read error: {:?}", e);
-                    break;
-                }
-            };
-        }
-
-        let r = socket
-            .write_all(
-                b"HTTP/1.0 200 OK\r\n\r\n\
-            <html>\
-                <body>\
-                    <h1>Hello Rust! Hello esp-wifi!</h1>\
-                </body>\
-            </html>\r\n\
-            ",
-            )
-            .await;
-        if let Err(e) = r {
-            println!("write error: {:?}", e);
-        }
+        connected_loop(&mut socket, &mut receiver).await;
 
         let r = socket.flush().await;
         if let Err(e) = r {
