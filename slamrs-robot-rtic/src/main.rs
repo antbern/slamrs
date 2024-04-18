649f3342905501a7084a20cbfa5ca4150ac7bfd5
antbern <40672068+antbern@users.noreply.github.com>
2024-04-18 22:09:36 +0200

    add hardware task for reading from ESP uart

--- slamrs-robot-rtic/src/main.rs
+++ slamrs-robot-rtic/src/main.rs
@@ -1,6 +1,5 @@
 #![no_main]
 #![no_std]
-// #![feature(type_alias_impl_trait)]
 
 // use rp_pico::hal as _;
 use defmt_rtt as _;
@@ -16,15 +15,30 @@ use panic_probe as _;
     peripherals = true
 )]
 mod app {
+    use defmt::{error, info, warn};
+
     use embedded_hal::digital::v2::{OutputPin, ToggleableOutputPin};
+
     use rp_pico::hal::{
-        clocks,
-        gpio::{self, bank0::Gpio10, FunctionSioOutput, PullDown},
+        self, clocks,
+        fugit::{ExtU64, RateExtU32},
+        gpio::{
+            self,
+            bank0::{Gpio10, Gpio19, Gpio24, Gpio4, Gpio5},
+            FunctionSioOutput, PullDown,
+        },
         sio::Sio,
+        uart::{DataBits, ReadErrorType, Reader, StopBits, Writer},
         watchdog::Watchdog,
+        Clock,
     };
     use rp_pico::XOSC_CRYSTAL_FREQ;
     use rtic_monotonics::rp2040::*;
+
+    type Uart1Pins = (
+        hal::gpio::Pin<Gpio4, hal::gpio::FunctionUart, hal::gpio::PullNone>,
+        hal::gpio::Pin<Gpio5, hal::gpio::FunctionUart, hal::gpio::PullNone>,
+    );
 
     // Shared resources go here
     #[shared]
@@ -37,6 +51,14 @@ mod app {
     struct Local {
         // TODO: Add resources
         led: gpio::Pin<Gpio10, FunctionSioOutput, PullDown>,
+
+        // the uart reader part used in the IRQ hardware task
+        uart1_rx: Reader<hal::pac::UART1, Uart1Pins>,
+        uart1_tx: Writer<hal::pac::UART1, Uart1Pins>,
+
+        // pins used to reset the ESP
+        esp_mode: gpio::Pin<Gpio24, FunctionSioOutput, PullDown>,
+        esp_reset: gpio::Pin<Gpio19, FunctionSioOutput, PullDown>,
     }
 
     #[init]
@@ -47,7 +69,7 @@ mod app {
             rp_pico::hal::sio::spinlock_reset();
         }
 
-        defmt::info!("init");
+        info!("init");
 
         // TODO setup monotonic if used
         // Initialize the interrupt for the RP2040 timer and obtain the token
@@ -56,7 +78,7 @@ mod app {
         // Configure the clocks, watchdog - The default is to generate a 125 MHz system clock
         Timer::start(ctx.device.TIMER, &mut ctx.device.RESETS, rp2040_timer_token); // default rp2040 clock-rate is 125MHz
         let mut watchdog = Watchdog::new(ctx.device.WATCHDOG);
-        let _clocks = clocks::init_clocks_and_plls(
+        let clocks = clocks::init_clocks_and_plls(
             XOSC_CRYSTAL_FREQ,
             ctx.device.XOSC,
             ctx.device.CLOCKS,
@@ -70,16 +92,42 @@ mod app {
 
         // Init LED pin
         let sio = Sio::new(ctx.device.SIO);
-        let gpioa = rp_pico::Pins::new(
+        let pins = hal::gpio::Pins::new(
             ctx.device.IO_BANK0,
             ctx.device.PADS_BANK0,
             sio.gpio_bank0,
             &mut ctx.device.RESETS,
         );
-        let mut led = gpioa.gpio10.into_push_pull_output();
+        let mut led = pins.gpio10.into_push_pull_output();
         led.set_low().unwrap();
 
-        task1::spawn().ok();
+        // initialize the pins for resetting the ESP, with HIGH as their current state
+        let esp_reset = pins
+            .gpio19
+            .into_push_pull_output_in_state(gpio::PinState::High);
+        let esp_mode = pins
+            .gpio24
+            .into_push_pull_output_in_state(gpio::PinState::High);
+
+        // init the UART
+        let uart_pins: Uart1Pins = (
+            // UART TX (characters sent from RP2040) on pin 1 (GPIO0)
+            pins.gpio4.reconfigure(),
+            // UART RX (characters received by RP2040) on pin 2 (GPIO1)
+            pins.gpio5.reconfigure(),
+        );
+        let mut uart =
+            hal::uart::UartPeripheral::new(ctx.device.UART1, uart_pins, &mut ctx.device.RESETS)
+                .enable(
+                    hal::uart::UartConfig::new(115200.Hz(), DataBits::Eight, None, StopBits::One),
+                    clocks.peripheral_clock.freq(),
+                )
+                .unwrap();
+        uart.enable_rx_interrupt();
+
+        let (rx, tx) = uart.split();
+
+        init_esp::spawn().ok();
         heartbeat::spawn().ok();
         (
             Shared {
@@ -88,6 +136,10 @@ mod app {
             Local {
                 // Initialization of local resources go here
                 led,
+                uart1_rx: rx,
+                uart1_tx: tx,
+                esp_mode,
+                esp_reset,
             },
         )
     }
@@ -95,17 +147,92 @@ mod app {
     // Optional idle, can be removed if not needed.
     // #[idle]
     // fn idle(_: idle::Context) -> ! {
-    //     defmt::info!("idle");
+    //     info!("idle");
     //
     //     loop {
     //         continue;
     //     }
     // }
 
-    // TODO: Add tasks
-    #[task(priority = 1)]
-    async fn task1(_cx: task1::Context) {
-        defmt::info!("Hello from task1!");
+    /// Task that initializes and handles the ESP wifi connection
+    #[task(priority = 1, local = [esp_mode, esp_reset, uart1_tx])]
+    async fn init_esp(cx: init_esp::Context) {
+        info!("Reseting the ESP");
+        cx.local.esp_mode.set_high().ok();
+        cx.local.esp_reset.set_low().ok();
+
+        Timer::delay(1.secs()).await;
+        cx.local.esp_reset.set_high().ok();
+        Timer::delay(1.secs()).await;
+
+        info!("Done");
+
+        cx.local.uart1_tx.write_full_blocking(b"AT\r\n");
+    }
+
+    /// Hardware task that reads bytes from the UART an publishes messages!
+    #[task(binds = UART1_IRQ, local = [uart1_rx])]
+    fn uart1_esp32(cx: uart1_esp32::Context) {
+        let mut buf = [0; 256];
+        let mut index = 0;
+
+        match cx.local.uart1_rx.read_raw(&mut buf) {
+            Ok(n) => {
+                // info!("RX: '{}'", core::str::from_utf8(&buf[..n]).unwrap());
+                index += n;
+
+                if index >= buf.len() - 1 {
+                    warn!("Buffer reached end without finding a newline!");
+                }
+
+                loop {
+                    // info!(
+                    //     "Index: {}, Buffer: '{}'",
+                    //     index,
+                    //     from_utf8(&buf[..index]).unwrap()
+                    // );
+                    let mut found = false;
+
+                    for i in 0..index.saturating_sub(1) {
+                        if buf[i] == b'\r' && buf[i + 1] == b'\n' {
+                            // found!, extract without the \r\n
+                            let cmd = &buf[0..i];
+
+                            // search the recently added bytes to find "\r\n"
+                            info!(
+                                "FOUND \\r\\n at {}: '{}'",
+                                i,
+                                core::str::from_utf8(&cmd).unwrap()
+                            );
+
+                            // reset the buffer by moving the remaining bytes to the front
+                            let first_other_byte = i + 2;
+                            // info!("copy range: {}", first_other_byte..index);
+                            buf.copy_within(first_other_byte..index, 0);
+                            index = index - first_other_byte;
+
+                            found = true;
+                            break; // break the for loop and restart it
+                        }
+                    }
+
+                    if !found {
+                        // break the outer loop if none was found
+                        break;
+                    }
+                }
+            }
+            Err(nb::Error::WouldBlock) => info!("Would Block"),
+            Err(nb::Error::Other(e)) => {
+                match e.err_type {
+                    ReadErrorType::Overrun => error!("Overrun"),
+                    ReadErrorType::Break => error!("Break"),
+                    ReadErrorType::Parity => error!("Parity"),
+                    ReadErrorType::Framing => error!("Framing"),
+                };
+                error!("Data: '{}'", core::str::from_utf8(&e.discarded).unwrap());
+            }
+        }
     }
 
     #[task(local = [led])]
