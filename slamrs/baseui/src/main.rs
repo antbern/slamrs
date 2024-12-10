38ab9adb4c64f19bf397050d03d4bc6007d77640
antbern <40672068+antbern@users.noreply.github.com>
2024-12-10 22:36:38 +0100

    update egui to `v0.29`

--- slamrs/baseui/src/main.rs
+++ slamrs/baseui/src/main.rs
@@ -55,6 +55,8 @@ fn main() -> Result<(), eframe::Error> {
 #[cfg(target_arch = "wasm32")]
 fn main() {
     use baseui::config::Config;
+    use eframe::wasm_bindgen::JsCast as _;
+    use eframe::web_sys;
 
     // Redirect `log` message to `console.log` and friends:
     eframe::WebLogger::init(log::LevelFilter::Debug).ok();
@@ -67,16 +69,41 @@ fn main() {
     let web_options = eframe::WebOptions::default();
 
     wasm_bindgen_futures::spawn_local(async {
-        eframe::WebRunner::new()
+        let document = web_sys::window()
+            .expect("No window")
+            .document()
+            .expect("No document");
+
+        let canvas = document
+            .get_element_by_id("the_canvas_id")
+            .expect("Failed to find the_canvas_id")
+            .dyn_into::<web_sys::HtmlCanvasElement>()
+            .expect("the_canvas_id was not a HtmlCanvasElement");
+
+        let start_result = eframe::WebRunner::new()
             .start(
-                "the_canvas_id", // hardcode it
+                canvas,
                 web_options,
                 Box::new(|cc| {
                     set_style(&cc.egui_ctx);
                     Ok(Box::new(baseui::App::new(cc, config)))
                 }),
             )
-            .await
-            .expect("failed to start eframe");
+            .await;
+
+        // Remove the loading text and spinner:
+        if let Some(loading_text) = document.get_element_by_id("loading_text") {
+            match start_result {
+                Ok(_) => {
+                    loading_text.remove();
+                }
+                Err(e) => {
+                    loading_text.set_inner_html(
+                        "<p> The app has crashed. See the developer console for details. </p>",
+                    );
+                    panic!("Failed to start eframe: {e:?}");
+                }
+            }
+        }
     });
 }
